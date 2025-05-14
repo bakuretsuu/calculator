644ad50b182ebc352f6db2e8910f1bd618279ef7
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-05-15 05:38:19 +0800

    created initial calculator set up

--- javascript.js
+++ javascript.js
@@ -13,3 +13,28 @@ function multiply(a,b){
 function divide(a,b){
     return a / b;
 };
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+function operate(operator, firstNumber, secondNumber){
+    if (operator === "add"){
+        return add(firstNumber,secondNumber);
+    } else if (operator === "subtract"){
+        return subtract(firstNumber,secondNumber);
+    } else if (operator === "multiply"){
+        return multiply(firstNumber,secondNumber);
+    } else if (operator === "divide"){
+        return divide(firstNumber,secondNumber);
+    }
+}
+
+const buttonsContainer = document.querySelector(".buttons");
+
+ 
+
+  
+ 
+
+ 
